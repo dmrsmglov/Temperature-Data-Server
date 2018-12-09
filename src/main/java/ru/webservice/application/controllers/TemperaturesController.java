600f04c1d1014c45944e03469cfb5916e328a28d
dmrsmglov <damiron123@gmail.com>
2018-12-10 00:22:35 +0300

    delete unused import

--- src/main/java/ru/webservice/application/controllers/TemperaturesController.java
+++ src/main/java/ru/webservice/application/controllers/TemperaturesController.java
@@ -15,7 +15,6 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping("/temperatures")
